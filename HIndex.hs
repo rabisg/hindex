8e1fa72f0466b3c0fca02dc2cb13ec91662229e0
rabisg <guha.rabishankar@gmail.com>
2015-04-18 15:21:05 +0530

    Export definitions and helper method for config

--- HIndex.hs
+++ HIndex.hs
@@ -1,9 +1,15 @@
 module HIndex ( initIndex
               , put
               , flush
+              , getConfig
               , HIndexConfig(..)
+              , HIndex()
+              , HIndexValue(..)
               ) where
 
 import           HIndex.Init
 import           HIndex.Operations
 import           HIndex.Types
+
+getConfig :: HIndex a -> HIndexConfig
+getConfig = hConfig
