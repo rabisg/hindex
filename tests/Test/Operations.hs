746e3e7db4c47a38983437b8fb2b1d0e789e3eea
rabisg <guha.rabishankar@gmail.com>
2015-04-25 18:07:17 +0530

    Tests for deleted Docs

--- tests/Test/Operations.hs
+++ tests/Test/Operations.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Test.Operations where
+module Test.Operations ( tests
+                       , testsOnExistingIndex
+                       ) where
 
 import           HIndex
 import           HIndex.Internal
@@ -44,6 +46,19 @@ getFooTest ioIndex msg = testCase msg $ do
   assertEqual "docId should be same" docId documentId
   assertEqual "docId should be same" termFreq (TermFreq fooFreq)
 
+delDocTest :: IO (HIndex Int TermFreq) -> String -> TestTree
+delDocTest ioIndex msg = testCase msg $ do
+  index <- ioIndex
+  getBaz <- get index "baz"
+  getBar <- get index "bar"
+  assertBool "baz should have no results" (null getBaz)
+  assertBool "bar should not have the deleted document" (notMember getBar documentId')
+  where
+    notMember [] _ =  True
+    notMember (x:xs) deletedDocId = case x of
+      TermValue docId _ | docId == deletedDocId -> False
+                        | otherwise -> notMember xs docId
+
 tests :: TestTree
 tests = withResource init_ nothing $ \ioIndex ->
   testGroup "Basic Operations"
@@ -69,6 +84,12 @@ tests = withResource init_ nothing $ \ioIndex ->
        assertEqual "get <bar> should return aggregate results" (length vs) 2
        assertEqual "results should be sorted" (sort vs) vs
 
+  , testCase "delete should succeed" $ do
+       index <- ioIndex
+       delete index documentId'
+
+  , delDocTest ioIndex "deleted results should not be present when in memory"
+
   , testCase "flush should write files to disk" $ do
        index <- ioIndex
        flush index
@@ -77,6 +98,8 @@ tests = withResource init_ nothing $ \ioIndex ->
        assertFileExists "hint file should exist" $ baseDir </> "1.hint"
 
   , getFooTest ioIndex "get should return the values after flush"
+
+  , delDocTest ioIndex "Deleted documents should not be present after flush"
   ]
   where
     nothing = const $ return ()
@@ -86,12 +109,16 @@ tests = withResource init_ nothing $ \ioIndex ->
       assertBool msg fileExists
 
 testsOnExistingIndex :: TestTree
-testsOnExistingIndex = withResource init_ (const $ return ()) $ \ioIndex ->
+testsOnExistingIndex = withResource init_ destroy $ \ioIndex ->
   testGroup "Operations on previously saved index"
-  [ testCase "previously written segments should exist" $ do
+  [ testCase "Previously stored state should be retrieved" $ do
        index <- ioIndex
        activeSegs <- readMVar $ hActiveSegments index
-       assertBool "There should be existing segments" (not . M.null $ activeSegs)
+       deletedDocIds <- readMVar $ hDeletedDocs index
+       assertBool "Segments should be retrieved" (not . M.null $ activeSegs)
+       assertBool "Deleted documents should be retrieved" (not . null $ deletedDocIds)
 
   , getFooTest ioIndex "get should return the previously stored values"
+
+  , delDocTest ioIndex "Deleted documents should not be present"
   ]
